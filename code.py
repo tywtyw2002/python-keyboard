534d1cb56099569993bc9296524392eaee94cbce
Tianyi Wu <tywtyw2002@gmail.com>
2021-12-30 16:42:47 -0700

    Add config

--- code.py
+++ code.py
@@ -1,109 +1,94 @@
 
-from keyboard import *
+from PYKB import *
 
 
-keyboard = Keyboard()
+class CKeyboard(Keyboard):
+    def change_bt(self, n):
+        changed = False
+        if self.usb_status == 3:
+            self.usb_status = 1
+            changed = True
+        if n != self.ble_id:
+            changed = True
+            self.set_bt_id(n)
+            self.ble.name = "CosHiM KB-%02d" % n
+            self.advertisement.complete_name = self.ble.name
+            self.start_advertising()
+        elif not self.ble.connected and not self.ble._adapter.advertising:
+            self.start_advertising()
+
+        if changed:
+            self.on_device_changed("BT{}".format(n))
+
+
+keyboard = CKeyboard()
 
 ___ = TRANSPARENT
 BOOT = BOOTLOADER
 L1 = LAYER_TAP(1)
-L2D = LAYER_TAP(2, D)
-L3B = LAYER_TAP(3, B)
-LSFT4 = LAYER_MODS(4, MODS(LSHIFT))
-RSFT4 = LAYER_MODS(4, MODS(RSHIFT))
-L5S = LAYER_TAP(5, S)
+L2 = LAYER_TAP(2)
+
+FN2 =LAYER_TAP_TOGGLE(1)
+FN3 =LAYER_TAP_TOGGLE(3)
+
+# LSFT4 = LAYER_MODS(4, MODS(LSHIFT))
+# RSFT4 = LAYER_MODS(4, MODS(RSHIFT))
 
 # Semicolon & Ctrl
-SCC = MODS_TAP(MODS(RCTRL), ';')
-SINS = MODS_KEY(MODS(SHIFT), INSERT)
+# SCC = MODS_TAP(MODS(RCTRL), ';')
+
+CALC = APPLAUNCH_CALCULATOR
+
+VOLU = 0x80  # Keyboard Volume Up
+VOLD = 0x81  # Keyboard Volume Down
 
 keyboard.keymap = (
     # layer 0
     (
         ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, '-', '=', BACKSPACE,
         TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, '[', ']', '|',
-        CAPS,  A,   S, L2D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
-        LSFT4, Z,   X,   C,   V, L3B,   N,   M, ',', '.', '/',         RSFT4,
-        LCTRL, LGUI, LALT,          SPACE,            RALT, MENU,  L1, RCTRL
+        LCTRL,  A,   S, D,   F,   G,   H,   J,   K,   L, ';', '"',    ENTER,
+        LSHIFT, Z,   X,   C,   V,  B,   N,   M, ',', '.', '/',         RSHIFT,
+        LCTRL, LALT, LGUI,          SPACE,            RGUI,  L1, L2, GRAVE
     ),
 
     # layer 1
     (
-        '`',  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,
-        ___, ___,  UP, ___, ___, ___, ___, ___, ___, ___,SUSPEND,___,___,___,
-        ___,LEFT,DOWN,RIGHT,___, ___, ___, ___, ___, ___, ___, ___,      ___,
-        ___, ___, ___, ___, ___,BOOT, ___,MACRO(0), ___, ___, ___,       ___,
+        GRAVE,  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,
+        ___, ___,  UP, ___, ___, ___, CALC, ___, INSERT, ___,PRTSCN,SCROLLLOCK,PAUSE,___,
+        ___,LEFT,DOWN,RIGHT,___, ___, ___, ___, ___, ___, HOME, PGUP ,      ___,
+        ___, ___, MENU, ___, ___, ___, VOLD,VOLU, MUTE, END, PGDN,       ___,
         ___, ___, ___,                ___,               ___, ___, ___,  ___
     ),
 
     # layer 2
     (
-        '`',  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,
-        ___, ___, ___, ___, ___, ___,HOME,PGUP, ___, ___,SINS,AUDIO_VOL_DOWN,AUDIO_VOL_UP,AUDIO_MUTE,
-        ___, ___, ___, ___, ___, ___,LEFT,DOWN, UP,RIGHT, ___, ___,      ___,
-        ___, ___, ___, ___, ___, ___,PGDN,END, ___, ___, ___,           ___,
-        ___, ___, ___,                ___,               ___, ___, ___,  ___
+        NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO ,
+        NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO ,
+        NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO , NO ,      NO ,
+        NO , NO , NO , NO , NO , FN3, NO , NO , NO , NO , NO ,           NO ,
+        NO , NO , NO ,                    FN2,               NO , NO , NO ,  NO 
     ),
 
     # layer 3
     (
-        BT_TOGGLE,BT1,BT2, BT3,BT4,BT5,BT6,BT7, BT8, BT9, BT0, ___, ___, ___,
-        RGB_MOD, ___, ___, ___, ___, ___,___,USB_TOGGLE,___,___,___,___,___, ___,
-        RGB_TOGGLE,HUE_RGB,RGB_HUE,SAT_RGB,RGB_SAT,___,___,___,___,___,___,___,      ___,
-        ___, ___, ___, ___, ___, ___, ___, ___,VAL_RGB,RGB_VAL, ___,           ___,
-        ___, ___, ___,                ___,               ___, ___, ___,  ___
-    ),
-
-    # layer 4
-    (
-        '`', ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___,   D, ___, ___, ___, ___, ___, ___, ';', ___,      ___,
-        ___, ___, ___, ___, ___,   B, ___, ___, ___, ___, ___,           ___,
-        ___, ___, ___,                ___,               ___, ___, ___,  ___
-    ),
-
-    # layer 5
-    (
-        ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___,MS_W_UP,MS_UL,MS_UP,MS_UR, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___,MS_BTN1,MS_LT,MS_DN,MS_RT,MS_BTN2, ___,      ___,
-        ___, ___, ___, ___, ___, ___,MS_W_DN,MS_DL,MS_DN,MS_DR, ___,           ___,
-        ___, ___, ___,                ___,               ___, ___, ___,  ___
+        BT_TOGGLE,BT1,BT2, BT3,BT4,BT5,BT6,BT7, BT8, BT9, BT0, NO , NO , NO ,
+        NO , NO , NO , NO , NO , NO ,NO ,USB_TOGGLE,NO ,NO ,SHUTDOWN ,NO ,NO , NO ,
+        NO , NO , SUSPEND, NO , NO , NO , NO , NO , NO , NO , NO , NO ,      NO ,
+        NO , NO , NO , NO , NO , MACRO(10) , NO , NO , NO , NO , NO ,           NO ,
+        NO , NO , NO ,                FN3 ,               NO , NO , NO ,  NO 
     ),
 )
 
-# Use different keymaps on different connections
-# Valid keys are "USB" and "BT0"-"BT9"
-# Connection not in this map will use default keymap defined above.
-keyboard.profiles = {
-    # For example, BT8 is connected to a Mac
-    "BT8": (
-        # layer 0
-        (
-            ESC,   1,   2,   3,   4,   5,   6,   7,   8,   9,   0, '-', '=', BACKSPACE,
-            TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P, '[', ']', '|',
-            CAPS,  A,   S,   D,   F,   G,   H,   J,   K,   L, SCC, '"',    ENTER,
-            LSHIFT,Z,   X,   C,   V,   B,   N,   M, ',', '.', '/',        RSHIFT,
-            LCTRL, LALT, LGUI,          SPACE,            MENU, RALT,  L1, RCTRL
-        ),
-
-        # layer 1
-        (
-            '`',  F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11, F12, DEL,
-            ___, ___,  UP, ___, ___, ___, ___, ___, ___, ___,SUSPEND,___,___,___,
-            ___,LEFT,DOWN,RIGHT,___, ___, ___, ___, ___, ___, ___, ___,      ___,
-            ___, ___, ___, ___, ___,BOOT, ___,MACRO(1), ___, ___, ___,       ___,
-            ___, ___, ___,                ___,               ___, ___, ___,  ___
-        ),
-    )
-}
 
 def macro_handler(dev, n, is_down):
     if is_down:
         dev.send_text('You pressed macro #{}\n'.format(n))
     else:
-        dev.send_text('You released macro #{}\n'.format(n))
+        if n == 10:
+            dev.send_text('Battary Lvl: {}%'.format(battery_level()))
+        else:
+            dev.send_text('You released macro #{}\n'.format(n))
 
 def pairs_handler(dev, n):
     dev.send_text('You just triggered pair keys #{}\n'.format(n))
@@ -112,15 +97,15 @@ def pairs_handler(dev, n):
 keyboard.macro_handler = macro_handler
 keyboard.pairs_handler = pairs_handler
 
-# ESC(0)    1(1)   2(2)   3(3)   4(4)   5(5)   6(6)   7(7)   8(8)   9(9)   0(10)  -(11)  =(12)  BACKSPACE(13)
-# TAB(27)   Q(26)  W(25)  E(24)  R(23)  T(22)  Y(21)  U(20)  I(19)  O(18)  P(17)  [(16)  ](15)   \(14)
-# CAPS(28)  A(29)  S(30)  D(31)  F(32)  G(33)  H(34)  J(35)  K(36)  L(37)  ;(38)  "(39)      ENTER(40)
-#LSHIFT(52) Z(51)  X(50)  C(49)  V(48)  B(47)  N(46)  M(45)  ,(44)  .(43)  /(42)            RSHIFT(41)
-# LCTRL(53)  LGUI(54)  LALT(55)               SPACE(56)          RALT(57)  MENU(58)  Fn(59)  RCTRL(60)
-
 # Pairs: J & K, U & I
 keyboard.pairs = [{35, 36}, {20, 19}]
 
-# keyboard.verbose = False
+keyboard.verbose = True
+
+# hot fix ble name
+bt_idx = int(keyboard.ble.name.replace("PYKB ",""))
+keyboard.ble.name = "CosHiM KB-%02d" % bt_idx
+keyboard.advertisement.complete_name = keyboard.ble.name
+
 
 keyboard.run()
